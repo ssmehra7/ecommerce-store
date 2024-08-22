16955bb91cbf1c03cd35f4459a7f6e7fc75c5e61
simarjeet <simarjeet.sm7@gmail.com>
2024-08-22 16:48:01 +0530

    Build the app

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -5,42 +5,34 @@ import { ProductList } from "@/components/product-list";
 import { Container } from "@/components/ui/container";
 import { Info } from "@/components/ui/info";
 
-
-
-
-interface ProductPageProps{
-    params:{
-        productId:string; 
+interface ProductPageProps {
+    params: {
+        productId: string;
     }
 }
 
-export const ProductPage:React.FC<ProductPageProps> = async({
-    params
-})=>{
-
-    const product  = await getProduct(params.productId);
+const ProductPage = async ({ params }: ProductPageProps) => {
+    const product = await getProduct(params.productId);
     const suggestedProducts = await getProducts({
-        categoryId:product?.category?.id,
-    })
-    
-    return(
+        categoryId: product?.category?.id,
+    });
+
+    return (
         <div>
             <Container>
                 <div className="px-4 py-10 sm:px-6 lg:px-8">
                     <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-                        <Gallery images={product.images}/>
+                        <Gallery images={product.images} />
                         <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-                            <Info data={product}/>
+                            <Info data={product} />
                         </div>
                     </div>
-                    <hr className="my-10"/>
-                    <ProductList title="Related Items" items={suggestedProducts}/>
-
+                    <hr className="my-10" />
+                    <ProductList title="Related Items" items={suggestedProducts} />
                 </div>
             </Container>
         </div>
-    )
-}
+    );
+};
 
-
-export default ProductPage; 
+export default ProductPage;
